f23672b9bb7f0541cc5d90be0ff7ee82badd34c1
ElijahTan <elijahtan@ElijahTandeiMac.local>
2024-08-07 10:08:31 +0800

    [solved]

--- DemoCommand/main.swift
+++ DemoCommand/main.swift
@@ -9,5 +9,26 @@ import Foundation
 
 
 
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+class Solution {
+    func swapPairs(_ head: ListNode?) -> ListNode? {
+        guard let head, let sec = head.next else {
+            return head
+        }
+        let third = sec.next
+        sec.next = head
+        head.next = swapPairs(third)
+        return sec
+    }
+}
+
+
 
 
